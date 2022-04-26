7ee4bf55ca04dc6d49f58cc17370eeca61229ccf
Vishal Kumar <vishalimp304@gmail.com>
2022-04-26 19:58:01 -0400

    Update Map.py

--- Map.py
+++ Map.py
@@ -4,7 +4,7 @@ from contextlib import contextmanager
 from contextlib import ExitStack
 
 '''
-Usage: Map.mapData('file_name.txt')
+Usage: Map.mapData('file_name',task_id)
 '''
 
 class Map():
@@ -12,27 +12,25 @@ class Map():
   #Have no of reducers here
   no_of_reducers = 3
 
-  def getFileNames(self, file_name: str):
-    file_names = [(file_name,'r')]
+  def getFileNames(self, file_name: str, task_id: int):
+    file_names = [(file_name+'_'+str(task_id),'r')]
     for i in range(Map.no_of_reducers):
       file_names.append(('reduce_output_'+str(i+1),'a'))
     return file_names
 
 
   @staticmethod
-  def mapData(file_name: str):
+  def mapData(file_name: str, task_id: int):
     # First read data from file, then map and then write based on consistent hashing.
     # For conistent hashing we need to cautios as we want to write data to predeined no of files
     # If the file is not there then open the file and append it.
-
     # Get filenames based on no of reducers and the input file name.
     m = Map()
-    file_names = m.getFileNames(file_name)
+    file_names = m.getFileNames(file_name, task_id)
     print(file_names)
     with ExitStack() as stack:
       files = [stack.enter_context(open(f_name, f_mode)) for f_name,f_mode in file_names]
       r_file = files[0]
-      print(r_file)
       for lines in r_file:
         words = re.findall('\w+',lines)
         for word in words:
